a14553c862c64f2a28d3b2c14ef41f5fc7d2fb26
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2024-10-02 17:25:19 +0000

    oinwaraw

--- PP1_10.py
+++ PP1_10.py
@@ -2,25 +2,21 @@ import math
 
 def q1(): 
   #Write Assignment code here
-  root = float(input("Input a number: "))
-  print(math.sqrt(root))
+  root = float(input("Input a number: ")); print(math.sqrt(root))
 
 
 def q2(): 
   #Write Assignment code here
-  introot = int(input("Input a number: "))
-  print(math.isqrt(introot))    
+  introot = int(input("Input a number: ")); print(math.isqrt(introot))    
 
 def q3(): 
   #Write Assignment code here
-  floor = float(input("Input a number: "))
-  print (math.floor(floor))
+  floor = float(input("Input a number: ")); print (math.floor(floor))
   
 
 def q4(): 
   #Write Assignment code here
-  Ceiling = float(input("Input a number: "))
-  print (math.ceil(Ceiling))
+  Ceiling = float(input("Input a number: ")); print (math.ceil(Ceiling))
 
 
 def q5(): 
@@ -33,8 +29,8 @@ def q5():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-# q1()
-# q2()
-# q3()
-# q4()
-# q5()
+q1()
+q2()
+q3()
+q4()
+q5()
