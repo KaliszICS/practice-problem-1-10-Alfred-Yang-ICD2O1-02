f3ba2deec2e21d9e212a0c55cb20b70a419fe17e
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2024-10-02 17:18:46 +0000

    fwaubgfawl

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,40 @@
-
+import math
 
 def q1(): 
   #Write Assignment code here
+  root = float(input("Input a number: "))
+  print(math.sqrt(root))
 
 
 def q2(): 
   #Write Assignment code here
-
+  introot = int(input("Input a number: "))
+  print(math.isqrt(introot))    
 
 def q3(): 
   #Write Assignment code here
-
+  floor = float(input("Input a number: "))
+  print (math.floor(floor))
+  
 
 def q4(): 
   #Write Assignment code here
+  Ceiling = float(input("Input a number: "))
+  print (math.ceil(Ceiling))
 
 
 def q5(): 
   #Write Assignment code here
-
-
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input another number: "))
+  sum = num1 * num2 / 2
+  print (math.trunc (sum))
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
